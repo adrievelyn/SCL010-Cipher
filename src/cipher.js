b9065db84463bfea2a8163023214523a705f77e7
Adriana Sanchez <adrievelyn@gmail.com>
2019-06-22 14:35:13 -0400

    se sube codigo funcional

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,77 @@
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (textBoxContent, offset) => {
     
+    let output = '';
+    // iteramos sobre la palabra con un loop for
+    //hasta la longitud de ella
+    for (let i = 0; i < textBoxContent.length;i++) {
+      // variable en donde se convierte cada letra en codigo ascii
+      let letterCode = textBoxContent.charCodeAt(i);
+      // variable vacia
+      let letterCodeTmp = '';
+      // analizando las letras mayusculas A= 65 Y Z= 90
+      if ((letterCode >= 65) && (letterCode <= 90))  {//&& que ambas condiciones se cumplan
+        
+        // ejecutando el offset y convirtiendo de lenguaje ascii a letra
+        //si se cumple la condicion se aplica la fórmula y la pasa a letra
+        //todo lo ejecutado en el codigo se guarda en el condigo temporal
+        letterCodeTmp = String.fromCharCode(((letterCode - 65 + offset) % 26) + 65);
+        
+        // analizando las letras minusculas a= 97 y z=122 y ejecutando los 33 espacios
+      } else if ((letterCode >= 97) && (letterCode <= 122)){
+       
+        letterCodeTmp = String.fromCharCode(((letterCode - 97 + offset) % 26) + 97);
+
+      } else if(letterCode >= 33 && letterCode <= 64){
+        letterCodeTmp = String.fromCharCode(((letterCode - 33 + offset) % 32) + 33);
+
+      } else if (letterCode === 32){
+        letterCodeTmp = String.fromCharCode(letterCode);
+
+      } else if (letterCode === 241 || letterCode === 209){
+        letterCodeTmp = String.fromCharCode(letterCode);
+      }
+
+  
+      // salida de la palabra encriptada concatena
+      //es una variable vacia a la que se concatena el resultado del encriptado
+      // es decir se le agrega letra por letra
+      output += letterCodeTmp;
+    }
+    return output;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (textBoxContent, offset) => {    
+
+    let output = '';
+    for (let i = 0; i < textBoxContent.length;i++) {
+      // variable en donde se convierte cada letra en codigo ascii
+      let letterCode = textBoxContent.charCodeAt(i);
+      // variable que guarda la conversion de codigo ascii a letra
+      let letterCodeTmp = '';
+      // analizando las letras mayusculas A= 65 Y Z= 90
+      if ((letterCode >= 65) && (letterCode <= 90)) {
+        letterCodeTmp = String.fromCharCode(((letterCode - 65 - offset + 26) % 26) + 65);
+
+      } else if ((letterCode >= 97) && (letterCode <= 122)) {//  analizando las letras minusculas a= 97 y z=122 y ejecutando los 33 espacios
+        letterCodeTmp = String.fromCharCode(((letterCode - 97 - offset + 26) % 26) + 97);    
+
+      } else if(letterCode >= 33 && letterCode <= 64){
+        letterCodeTmp = String.fromCharCode(((letterCode - 33 - offset + 32) % 32) + 33);
+
+      } else if (letterCode === 32){
+        letterCodeTmp = String.fromCharCode(letterCode);
+
+      } else if (letterCode === 241 || letterCode === 209){
+          letterCodeTmp = String.fromCharCode(letterCode);
+      }
+
+      // salida de la palabra desencriptada concatenada
+      //es una variable vacia a la que se concatena el resultado de lo desencriptado
+      // es decir se le agrega letra por letra
+      output += letterCodeTmp;
+        
+    }
+    return output;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-/*Acá va tu código*/
+
+/* Realizado proyecto individual Cifrado Cesar window es un objeto DOM que representa todos los objetos en el HTML,
+le agregué un Listener para escuchar un evento, en este caso se usó el evento (load) para que
+se llame a la funciøn cuando se cargue la página, existen diversidad de eventos*/
+window.addEventListener('load', () => {
+  /* se CRean cuatro variables, va a contener los elementos que se le asignan con getelemnet a la variable creada */
+  let offset = document.getElementById("offSetBox");
+  let textBox = document.getElementById('textBox');
+  let encryptButton = document.getElementById('encryptButton');
+  let decryptButton = document.getElementById('decryptButton');
+  let resultBox = document.getElementById('resultBox');
+
+  /* se creó función para encriptar una palabra con un boton al que se le agregó Listener para escuchar un evento llamado click*/
+  encryptButton.addEventListener('click', () => {
+    let offsetNumber = parseInt(offset.value);
+    if (offsetNumber < 0) { alert("Ingrese un numero positivo"); }
+    let textBoxContent = textBox.value;//creamos una variable a la que se le asigna el valor (value) de la caja de texto
+    if (textBoxContent == '') { alert("Ingrese un texto a cifrar"); }
+    if (textBoxContent.match(/[a-zA-ZñÑ]/)) { //textBoxContent.match(/[a-zA-Z]/) define una expresion regular
+      let sendOffset = window.cipher.encode(textBoxContent,offsetNumber);
+//que indica que solo acepta letras de la a a la z minúsculas y mayúsculas
+//no acepta números y caratéres especiales (la funcion string.match indica que solo acepta una expresión regular que coincida con
+//lo que se esta definiendo
+      
+      /* Resultado*/
+      resultBox.classList.remove('hide');//removiendo la clase de nombre hide (oculto) al resultbox
+      resultBox.innerHTML = 'El texto encriptado es : ' + sendOffset ; // con inner se ingresa a resultbox lo que indica la variable output
+    }
+  });
+
+  /* se creó función para desencriptar una palabra con un boton al que se le agregó Listener para escuchar un evento llamado click*/
+  //sobre decryptButton
+  decryptButton.addEventListener('click', () => {
+    let offsetNumber = parseInt(offset.value);
+    if (offsetNumber < 0) { alert("Ingrese un numero positivo"); }
+    let textBoxContent = textBox.value; //creamos una variable a la que se le asigna el valor (value) de la caja de texto
+    if (textBoxContent == '') { alert("Ingrese un texto a cifrar"); }
+    if (textBoxContent.match(/[a-zA-ZñÑ]/)) { //textBoxContent.match(/[a-zA-Z]/) define una expresion regular
+      let sendOffset = window.cipher.decode(textBoxContent,offsetNumber);
+
+//que indica que solo acepta letras de la a a la z minúsculas y mayúsculas
+//no acepta números y caratéres especiales (la funcion string.match indica que solo acepta una expresión regular que coincida con
+//lo que se esta definiendo
+      
+      /* Resultado*/
+      resultBox.classList.remove('hide');//removiendo la clase de nombre hide (oculto) al resultbox
+      resultBox.innerHTML = 'El texto desencriptado es : ' + sendOffset ; // con inner se ingresa a resultbox lo que indica la variable output
+    }
+  });// con inner se ingresa a resultbox lo que indica la variable output
+  /* función para limpiar la entrada*/
+  let clear = document.getElementById('eraserButton');
+  clear.addEventListener('click', () => {
+  /* le asigna vaciocal elemento text box*/
+    textBox.value = '';
+    offset.value = '';
+  });
+});
